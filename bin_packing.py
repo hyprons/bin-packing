87560d39827757dce74f6e567fd3fc8a9951cf31
hyperons <118084923+hyprons@users.noreply.github.com>
2026-04-15 15:55:36 +0200

    chore: initial commit

---
+++ bin_packing.py
@@ -0,0 +1,97 @@
+def compute_rotation(dim: list[float], rot: tuple[int, int, int]) -> list[float]:
+    dim_new = dim.copy()
+
+    if rot[0]:  # x rot
+        temp = dim[2]
+        dim_new[2] = dim_new[1]
+        dim_new[1] = temp
+    if rot[1]:  # y rot
+        temp = dim[0]
+        dim_new[0] = dim_new[2]
+        dim_new[2] = temp
+    if rot[2]:  # z rot
+        temp = dim[0]
+        dim_new[0] = dim_new[1]
+        dim_new[1] = temp
+
+    return dim_new
+
+
+def compute_item_count(box_dim: list[float], item_dim: list[float], branch: int) -> int:
+    global rotations
+    global rotations_b
+
+    item_space = [
+        box_dim[0] // item_dim[0],
+        box_dim[1] // item_dim[1],
+        box_dim[2] // item_dim[2],
+    ]
+
+    diffs = [box_dim[i] - item_dim[i] * item_space[i] for i in range(0, 3)]
+    splits = [
+        [diffs[2], box_dim[0], box_dim[1]],
+        [diffs[0], (item_space[2] * item_dim[2]), box_dim[1]],
+        [diffs[1], (item_space[0] * item_dim[0]), (item_space[2] * item_dim[2])],
+    ]
+
+    count = int(item_space[0] * item_space[1] * item_space[2])
+
+    best_count_total = 0
+
+    for i in splits:
+        best_count = 0
+        sub_box_vol = i[0] * i[1] * i[2]
+        if sub_box_vol > 0:
+            for j in rotations:
+                rot_dim = compute_rotation(item_dim, j)
+                c = i[0] // rot_dim[0] * i[1] // rot_dim[1] * i[2] // rot_dim[2]
+                if c > 0:
+                    print(
+                        f"Split {i} found at depth {branch} with rotation {j}! {c} items can fit."
+                    )
+                    count_split = compute_item_count(
+                        i, compute_rotation(item_dim, j), branch + 1
+                    )
+                    if count_split > best_count:
+                        best_count = count_split
+        best_count_total += best_count
+    # mark shared space, if unused by one split, allocate to the other
+    count += best_count_total
+
+    return count
+
+
+rotations = [(0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), (1, 0, 1)]
+rotations_b = [(0, 0, 0)]
+
+box_dim = [float(i) for i in input("box dimension (x,y,z): ").split(",")]
+box_vol = box_dim[0] * box_dim[1] * box_dim[2]
+
+item_dim = [float(i) for i in input("item dimension (x,y,z): ").split(",")]
+item_vol = item_dim[0] * item_dim[1] * item_dim[2]
+
+max_count = box_vol // item_vol
+print(f"The maximum is {max_count} items")
+
+best_rotation = (0, 0, 0)
+best_eff = 0
+best_count = 0
+
+for i in rotations:
+    print(f"Trying rotation {i}")
+    item_count = compute_item_count(box_dim, compute_rotation(item_dim, i), 0)
+    print(f"{item_count} items can fit in the box")
+    print(f"{box_vol - item_count * item_vol} units of space will be left")
+
+    efficiency = item_count / max_count * 100
+    print(f"Packing efficiency of {item_count / max_count * 100}%")
+
+    if efficiency > best_eff:
+        best_eff = efficiency
+        best_rotation = i
+        best_count = item_count
+# TODO: prioritize answers with the least amount of branches
+print(
+    f"The most efficient rotation is x:{best_rotation[0] * 90}, y:{best_rotation[1] * 90}, z:{best_rotation[2] * 90} with an effiency of {best_eff}%"
+)
+print(f"It could fit a max of {best_count} items")
